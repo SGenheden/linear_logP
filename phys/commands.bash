5118e2a8070dd922b923e6f33c44ee7f8f081825
SGenheden <s.genheden@soton.ac.uk>
2017-12-27 14:55:04 +0100

    Update to latest model

--- phys/commands.bash
+++ phys/commands.bash
@@ -1,4 +1,4 @@
-#python compare_smi.py ../data.xlsx minnesota.xlsx 
+#python compare_smi.py ../data.xlsx minnesota.xlsx
 
 python $SCRIPTS/Plot/plot_xy.py -f linear.dat -l linear -o linear.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle
 
@@ -7,6 +7,9 @@ python $SCRIPTS/Plot/plot_xy.py -f linear.dat elba.dat -l Statistik Elba  -o lin
 
 python $SCRIPTS/Plot/plot_xy.py -f martini_hybrid.dat -l martini_hybrid -o martini_hybrid.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle
 python $SCRIPTS/Plot/plot_xy.py -f martini_cg.dat -l martini_cg -o martini_cg.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle
-python $SCRIPTS/Plot/plot_xy.py -f elba.dat martini_hybrid.dat martini_cg.dat  -l Elba Martini2 Martini1  -o martini_elba.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle
+python $SCRIPTS/Plot/plot_xy.py -f elba.dat martini_cg.dat  -l Elba Martini  -o martini_elba.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle
+python $SCRIPTS/Plot/plot_xy.py -f elba.dat martini_cg.dat  -l Elba Martini  -o martini_elba.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle --ymax 12 --ymin -4
 
 python $SCRIPTS/Plot/plot_xy.py -f elba_water.dat martini_water.dat  -l Elba Martini2 -o martini_elba_water.png --correlation --xlabel="Experiment [kcal/mol]" --ylabel="Modell [kcal/mol]"  --scatter --nostyle
+
+python $SCRIPTS/Plot/plot_xy.py -f bench.dat bench.dat bench.dat  -l  Slipids Charmm36 Gaff --multicol 2 3 4 -o bench.png --correlation --xlabel="Experiment" --ylabel="Modell"  --scatter --nostyle
--- props/collect_props.py
+++ props/collect_props.py
@@ -5,6 +5,7 @@ Calculates the properties with the Indigo package
 from __future__ import division, print_function, absolute_import
 
 import sys
+import urllib
 
 import openpyxl as xl
 from indigo import Indigo
@@ -23,6 +24,64 @@ electronegativity = {1:2.3 , 2:4.16 , 3:0.912 , 4:1.576 , 5:2.051 , 6:2.544 ,
                     55:0.659 , 56:0.881 , 71:1.09 , 72:1.16 , 73:1.34 , 74:1.47 ,
                     75:1.6 , 76:1.65 , 77:1.68 , 78:1.72 , 79:1.92 , 80:1.76 ,
                     81:1.789 , 82:1.854 , 83:2.01 , 84:2.19 , 85:2.39 , 86:2.6 , 87:0.67 , 88:0.89}
+
+# Atomic masses
+atomic_mass = [0.0, 1.0079, 4.0026, 6.941, 9.0122, 10.811, 12.0107, 14.0067, 15.9994, 18.9984,
+        20.1797, 22.9898, 24.305, 26.9815, 28.0855, 30.9738, 32.065, 35.453, 39.948,
+        39.0983, 40.078, 44.9559, 47.867, 50.9415, 51.9961, 54.938, 55.845, 58.9331,
+        58.6934, 63.546, 65.409, 69.723, 72.64, 74.9216, 78.96, 79.904, 83.798, 85.4678,
+        87.62, 88.9059, 91.224, 92.9064, 95.94, 98.0, 101.07, 102.9055, 106.42, 107.8682,
+        112.411, 114.818, 118.71, 121.76, 127.6, 126.9045, 131.293, 132.9055,
+        137.327, 138.9055, 140.116, 140.9077, 144.242, 145.0, 150.36, 151.964,
+        157.25, 158.9254, 162.5, 164.9303, 167.259, 168.9342, 173.04, 174.967,
+        178.49, 180.9479, 183.84, 186.207, 190.23, 192.217, 195.084, 196.9666,
+        200.59, 204.3833, 207.2, 208.9804, 209.0, 210.0, 222.0, 223.0, 226.0,
+        227.0, 232.0381, 231.0359, 238.0289, 237.0, 244.0, 243.0, 247.0, 247.0,
+        251.0, 252.0]
+
+def smiles2file(smiles) :
+    """
+    Convert SMILES using Cactus service
+    Parameters
+    ----------
+    smiles : string
+      the SMILES string
+    Returns
+    -------
+    string
+      the path to the downloaded SDF file
+    Raises
+    ------
+    Exception
+      if the retrieval fails
+    """
+    smiles = smiles.replace("#","%23")
+    smiles = smiles.replace("+","%2B")
+    site = "cactus.nci.nih.gov"
+    try:
+        page = urllib.urlopen("http://%s/cgi-bin/translate.tcl?smiles=%s&format=mol&astyle=kekule&dim=3D&file="%(site, smiles))
+    except:
+        raise Exception("Could not connect with server")
+
+    for line in page:
+        if "Click here" in line  and 'a href="' in line :
+            dummy1, url, dummy2 = line.split('"')
+            try:
+                path, header = urllib.urlretrieve("http://%s%s"%(site,url))
+            except:
+                raise Exception("Could not retrieve file")
+            return path
+
+def gravitational_index(mol) :
+
+    gi = 0
+    for bond in mol.iterateBonds() :
+        xyz1 = bond.source().xyz()
+        xyz2 = bond.destination().xyz()
+        d = (xyz1[0]-xyz2[0])**2 + (xyz1[1]-xyz2[1])**2 + (xyz1[2]-xyz2[2])**2
+        gi += (atomic_mass[bond.source().atomicNumber()]* \
+                atomic_mass[bond.destination().atomicNumber()]) / d
+    return gi
 
 def hbond_donors(mol) :
 
@@ -70,6 +129,7 @@ ws["H1"] = "Polar"
 ws["I1"] = "Rings"
 ws["J1"] = "HDonors"
 ws["K1"] = "HAcceptors"
+ws["L1"] = "GI"
 
 i = 2
 while True :
@@ -84,6 +144,17 @@ while True :
     ws.cell(row=i, column=9).value = mol.countSSSR()
     ws.cell(row=i, column=10).value = hbond_donors(mol)
     ws.cell(row=i, column=11).value = hbond_acceptors(mol)
+
+
+    for d in range(5) : # Try 5 times before giving up
+        path = path = smiles2file(smi)
+        if path is not None :
+            break
+    if path is not None :
+        mol2 = ind.loadMoleculeFromFile(path)
+        ws.cell(row=i, column=12).value = gravitational_index(mol2)
+    else :
+        ws.cell(row=i, column=12).value = -1
     i += 1
 
 wb.save(sys.argv[1])
